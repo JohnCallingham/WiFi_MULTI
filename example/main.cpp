e04e12b70f94e41ecf281b4232276b542c363b87
JohnCallingham <JohnCallingham@users.noreply.github.com>
2025-06-22 20:11:06 +0100

    Added example

---
+++ example/main.cpp
@@ -0,0 +1,51 @@
+/**
+ * Wifi_Multi
+ * A way to allow an esp32 to automatically connect to different WiFi APs depending on location. 
+ * Keep a json format file credentials.h with name, ssid and password combinations.  Poss one is a favourite.
+ * On start up;-
+ *  1. Do a wifi scan and return all ssids in range.
+ *  2. Check the json file to see if any ssid in range is in the file.
+ *  3. If so, use its password to connect.
+ */
+#include <Arduino.h>
+#include <WiFi.h>
+#include "WiFi_Multi.h"
+#include "credentials.h"
+
+WiFi_Multi wifi_Multi;
+
+void setup() {
+  Serial.begin(115200);
+
+  delay(5000);
+
+  Serial.printf("\n%6ld Started program", millis());
+
+  WiFi_Multi_Error error = wifi_Multi.findMatchingSSID(credentials);
+  if (error) {
+    // Display error and exit program.
+    Serial.printf("\n%6ld Error finding matching SSID: %s", millis(), error.c_str());
+  } else {
+    // Match found. Use wifi_Multi.SSID and wifi_Multi.password to connect to the matching SSID.
+    const char* name = wifi_Multi.getMatchingName();
+    const char* ssid = wifi_Multi.getMatchingSSID();
+    const char* password = wifi_Multi.getMatchingPassword();
+
+    WiFi.setMinSecurity(WIFI_AUTH_WPA_PSK); // Default is WPA2
+
+    if( WiFi.status() != WL_CONNECTED ) {
+      Serial.printf("\n%6ld Starting connection to %s SSID: %s", millis(), name, ssid);
+
+      while (WiFi.status() != WL_CONNECTED) {
+        WiFi.begin(ssid, password);
+        delay(2000); // Originally 1000mS, but a second ESP32 didn't work until changed to 2000mS.
+        Serial.printf("\n%6ld Connecting to %s SSID: %s", millis(), name, ssid);
+      }
+      Serial.printf("\n%6ld Connected to %s SSID: %s", millis(), name, ssid);
+      Serial.printf("\n%6ld IP address: %s", millis(), WiFi.localIP().toString());
+    }
+  }
+}
+
+void loop() {
+}
