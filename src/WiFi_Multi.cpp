01b2983fdd492345257a8867286a87b40993ae2e
JohnCallingham <JohnCallingham@users.noreply.github.com>
2025-06-22 14:10:35 +0100

    Updated code

--- src/WiFi_Multi.cpp
+++ src/WiFi_Multi.cpp
@@ -3,60 +3,104 @@
 using namespace std;
 #include <WiFi.h>
 
-WiFi_Multi::WiFi_Multi(const char* credentials) {
-  this->credentials = credentials;
+// WiFi_Multi::WiFi_Multi(const char* credentials) {
+//   this->credentials = credentials;
  
-  // Deserialise the credentials JSON.
+//   // Deserialise the credentials JSON.
+//   Serial.printf("\n%6ld Deserialising credentials", millis());
+//   DeserializationError error = deserializeJson(doc, credentials);
+//   if (error) {
+//     Serial.printf("\n%6ld error deserialising credentials JSON: %s", millis(), error.f_str());
+//     // TO DO: throw an exception here.
+//   } else {
+//     Serial.printf("\n%6ld Successfully deserialised credentials", millis());
+//   }
+// }
+
+WiFi_Multi_Error WiFi_Multi::findMatchingSSID(const char* credentials) {
+  this->credentials = credentials;
+
+  WiFi_Multi_Error myError;
+
+  /**
+   * Deserialise the credentials JSON.
+   */
   Serial.printf("\n%6ld Deserialising credentials", millis());
   DeserializationError error = deserializeJson(doc, credentials);
   if (error) {
     Serial.printf("\n%6ld error deserialising credentials JSON: %s", millis(), error.f_str());
-    // TO DO: throw an exception here.
-  } else {
-    Serial.printf("\n%6ld Successfully deserialised credentials", millis());
+    myError.code = WiFi_Multi_Error::errorCode::DeserialisationError;
+    return myError;
   }
-}
+  Serial.printf("\n%6ld Successfully deserialised credentials", millis());
 
-WiFi_Multi_Error WiFi_Multi::findMatchingSSID(const char* credentials) {
-  WiFi_Multi_Error error;
+  /**
+   * Scan the WiFi networks.
+   */
+  Serial.printf("\n%6ld Scanning WiFi networks", millis());
 
-  // Deserialise the credentials JSON.
-
-  // Scan the WiFi networks.
-
-  // Check for an SSID found in the scan which we have a match for in the credentials.
-
-  // If an error occurs, store the cause and return. If no error, returen false.
-  
-  return error;
-
-}
-
-JsonObject WiFi_Multi::scanNetworks() {
   WiFi.mode(WIFI_STA);
 
   int n = WiFi.scanNetworks();
 
   if (n==0) {
     Serial.printf("\n%6ld No WiFi networks found", millis());
-    // TO DO: throw an exception here.
+    myError.code = WiFi_Multi_Error::errorCode::NoWiFiNetworks;
+    return myError;
   }
+  Serial.printf("\n%6ld Successfully scanned WiFi networks", millis());
+
+  /**
+   * Check for an SSID found in the scan which we have a match for in the credentials.
+   */
+  Serial.printf("\n%6ld Checking for matching SSID", millis());
 
   for (uint8_t i=0; i<n; i++) {
     for (JsonObject elem : doc.as<JsonArray>()) {
       if (strcmp(WiFi.SSID(i).c_str(), elem["ssid"]) == 0) {
-        return elem;
+        Serial.printf("\n%6ld Match found", millis());
+        // If a match has been found, then staore the ssis, etc in WiFi_Multi methods.
+        // Store the matching values.
+        this->foundName = elem["name"];
+        this->foundSSID = elem["ssid"];
+        this->foundPassword = elem["password"];
+
+        myError.code = WiFi_Multi_Error::errorCode::Ok;
+        return myError;
       }
     }
   }
-  throw invalid_argument("no matching SSID found in credentials");
+
+  // If an error occurs, store the cause and return. If no error, returen false.
+  myError.code = WiFi_Multi_Error::errorCode::NoMatch;
+  return myError;
 }
 
-void WiFi_Multi::printCredentials() {
-  const char *name;
+// JsonObject WiFi_Multi::scanNetworks() {
+//   WiFi.mode(WIFI_STA);
 
-  for (JsonObject elem : doc.as<JsonArray>()) {
-    name = elem["name"];
-    Serial.printf("\nWiFi_Multi name: %s", name);
-  }
-}
+//   int n = WiFi.scanNetworks();
+
+//   if (n==0) {
+//     Serial.printf("\n%6ld No WiFi networks found", millis());
+//     // TO DO: throw an exception here.
+//   }
+
+//   for (uint8_t i=0; i<n; i++) {
+//     for (JsonObject elem : doc.as<JsonArray>()) {
+//       if (strcmp(WiFi.SSID(i).c_str(), elem["ssid"]) == 0) {
+//         return elem;
+//       }
+//     }
+//   }
+//   throw invalid_argument("no matching SSID found in credentials");
+// }
+
+// void WiFi_Multi::printCredentials() {
+//   const char *name;
+
+//   for (JsonObject elem : doc.as<JsonArray>()) {
+//     name = elem["name"];
+//     Serial.printf("\nWiFi_Multi name: %s", name);
+//   }
+// }
--- src/WiFi_Multi.h
+++ src/WiFi_Multi.h
@@ -2,36 +2,56 @@
 
 class WiFi_Multi_Error {
   public:
+    enum errorCode {
+      Ok,
+      DeserialisationError,
+      NoWiFiNetworks,
+      NoMatch,
+      WiFiError //used ??
+    };
 
     // Returns true if there is an error
     explicit operator bool() const {
-      //return code_ != Ok;
-      return false;
+      return code != errorCode::Ok;
+      //return false;
   }
 
-  private:
+    errorCode code;
 
+  private:
+    //errorCode code;
 
 };
 
 class WiFi_Multi {
   public:
-    WiFi_Multi(const char* credentials);
+    //WiFi_Multi(const char* credentials);
     WiFi_Multi() {};
 
     WiFi_Multi_Error findMatchingSSID(const char* credentials);
 
-    void printCredentials();
+    const char* getFoundName() { return this->foundName; }
+    const char* getFoundSSID() { return this->foundSSID; }
+    const char* getFoundPassword() { return this->foundPassword; }
+
+
+    //void printCredentials();
 
     /**
      * Returns the matching JsonObject if ssid is in the credentials JSON.
      * Throws an invalid_argument exception if not found.
      */
-    JsonObject scanNetworks();
+    //JsonObject scanNetworks();
 
   private:
     const char* credentials;
 
     JsonDocument doc;
+  
+    WiFi_Multi_Error error;
+
+    const char* foundName;
+    const char* foundSSID;
+    const char* foundPassword;
 
 };
