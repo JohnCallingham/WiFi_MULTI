bfe06b9f0b978e99cb6fb8797f153e64d312cf9e
JohnCallingham <JohnCallingham@users.noreply.github.com>
2025-06-22 23:01:40 +0100

    Updated print messages

--- src/WiFi_Multi.cpp
+++ src/WiFi_Multi.cpp
@@ -7,40 +7,40 @@ WiFi_Multi_Error WiFi_Multi::findMatchingSSID(const char* credentials) {
   /**
    * Deserialise the credentials JSON.
    */
-  Serial.printf("\n%6ld Deserialising credentials", millis());
+  Serial.printf("\n%6ld WiFi_Multi: Deserialising credentials", millis());
   DeserializationError error = deserializeJson(doc, credentials);
   if (error) {
-    Serial.printf("\n%6ld Error deserialising credentials JSON: %s", millis(), error.f_str());
+    Serial.printf("\n%6ld WiFi_Multi: Error deserialising credentials JSON: %s", millis(), error.f_str());
     wifi_Multi_Error.returnCode = WiFi_Multi_Error::ReturnCode::DeserialisationError;
     return wifi_Multi_Error;
   }
-  Serial.printf("\n%6ld Successfully deserialised credentials", millis());
+  Serial.printf("\n%6ld WiFi_Multi: Successfully deserialised credentials", millis());
 
   /**
    * Scan the WiFi networks.
    */
-  Serial.printf("\n%6ld Scanning WiFi networks", millis());
+  Serial.printf("\n%6ld WiFi_Multi: Scanning WiFi networks", millis());
 
   WiFi.mode(WIFI_STA);
 
   int n = WiFi.scanNetworks();
 
   if (n==0) {
-    Serial.printf("\n%6ld No WiFi networks found", millis());
+    Serial.printf("\n%6ld WiFi_Multi: No WiFi networks found", millis());
     wifi_Multi_Error.returnCode = WiFi_Multi_Error::ReturnCode::NoWiFiNetworks;
     return wifi_Multi_Error;
   }
-  Serial.printf("\n%6ld Successfully scanned WiFi networks", millis());
+  Serial.printf("\n%6ld WiFi_Multi: Successfully scanned WiFi networks", millis());
 
   /**
    * Check for an SSID found in the scan which we have a match for in the credentials.
    */
-  Serial.printf("\n%6ld Checking for matching SSID", millis());
+  Serial.printf("\n%6ld WiFi_Multi: Checking for matching SSID", millis());
 
   for (uint8_t i=0; i<n; i++) {
     for (JsonObject elem : doc.as<JsonArray>()) {
       if (strcmp(WiFi.SSID(i).c_str(), elem["ssid"]) == 0) {
-        Serial.printf("\n%6ld Matching SSID found: %s", millis(), WiFi.SSID(i).c_str());
+        Serial.printf("\n%6ld WiFi_Multi: Matching SSID found: %s", millis(), WiFi.SSID(i).c_str());
         // If a match has been found, then store the matching name,
         //  SSID and password so it can be accessed later.
         this->matchingName = elem["name"];
