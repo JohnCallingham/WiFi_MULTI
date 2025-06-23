21c2e5be80cc562fe6ef17472b5241d80447f351
JohnCallingham <JohnCallingham@users.noreply.github.com>
2025-06-23 13:13:12 +0100

    Step through credentials first

--- src/WiFi_Multi.cpp
+++ src/WiFi_Multi.cpp
@@ -37,8 +37,9 @@ WiFi_Multi_Error WiFi_Multi::findMatchingSSID(const char* credentials) {
    */
   Serial.printf("\n%6ld WiFi_Multi: Checking for matching SSID", millis());
 
-  for (uint8_t i=0; i<n; i++) {
-    for (JsonObject elem : doc.as<JsonArray>()) {
+  // Step through the credentials as the outer loop so that APs higher up in the credentials list get priority.
+  for (JsonObject elem : doc.as<JsonArray>()) {
+    for (uint8_t i=0; i<n; i++) {
       if (strcmp(WiFi.SSID(i).c_str(), elem["ssid"]) == 0) {
         Serial.printf("\n%6ld WiFi_Multi: Matching SSID found: %s", millis(), WiFi.SSID(i).c_str());
         // If a match has been found, then store the matching name,
@@ -52,6 +53,22 @@ WiFi_Multi_Error WiFi_Multi::findMatchingSSID(const char* credentials) {
       }
     }
   }
+
+  // for (uint8_t i=0; i<n; i++) {
+  //   for (JsonObject elem : doc.as<JsonArray>()) {
+  //     if (strcmp(WiFi.SSID(i).c_str(), elem["ssid"]) == 0) {
+  //       Serial.printf("\n%6ld WiFi_Multi: Matching SSID found: %s", millis(), WiFi.SSID(i).c_str());
+  //       // If a match has been found, then store the matching name,
+  //       //  SSID and password so it can be accessed later.
+  //       this->matchingName = elem["name"];
+  //       this->matchingSSID = elem["ssid"];
+  //       this->matchingPassword = elem["password"];
+
+  //       wifi_Multi_Error.returnCode = WiFi_Multi_Error::ReturnCode::Ok;
+  //       return wifi_Multi_Error;
+  //     }
+  //   }
+  // }
 
   // No match was found, so set returnCode accordingly and return.
   wifi_Multi_Error.returnCode = WiFi_Multi_Error::ReturnCode::NoMatch;
